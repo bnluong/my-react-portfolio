2a96c6add0a779ae95fb6fdf5544d4a294476f10
Bao Luong <bnluong@uci.edu>
2021-01-07 17:47:29 -0800

    Finished up Projects, small tweaks on Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import './Footer.css';
+import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
 	return (
-		<div className='footer flex justify-center text-white py-8'>
-			<h1 className='lg:text-sm text-xs font-serif font-semibold'>
-				Copyright 2020 © Bao Luong. All rights reserved.
-			</h1>
+		<div className='footer flex justify-center mt-24 p-2'>
+			<div className='grid grid-rows-2'>
+				<div className='flex flex-row justify-center'>
+					<SocialIcon
+						url='https://linkedin.com/in/bao-luong'
+						style={{
+							maxWidth: '2rem',
+							maxHeight: '2rem',
+							marginTop: '0.5rem',
+							marginBottom: '0.5rem',
+							marginRight: '0.5rem',
+						}}
+					/>
+					<SocialIcon
+						url='https://github.com/bnluong'
+						style={{
+							maxWidth: '2rem',
+							maxHeight: '2rem',
+							margin: '0.5rem 0.5rem',
+						}}
+					/>
+					<SocialIcon
+						url='mailto:bnluong@uci.edu'
+						bgColor='#52afd3'
+						fgColor='#ffffff'
+						style={{
+							maxWidth: '2rem',
+							maxHeight: '2rem',
+							margin: '0.5rem 0.5rem',
+						}}
+					/>
+				</div>
+				<div className='mt-3 sm:text-base text-sm font-mono'>
+					Bao Luong © {new Date().getFullYear()}. All rights reserved.
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,25 +9,32 @@ function Projects() {
 			id='projects'
 		>
 			<div className='mx-3 md:mx-12 lg:mx-36'>
-				<div className='heading lg:py-12 mb-3'>
+				<div className='heading lg:py-12 py-3'>
 					<h1 className='text-2xl font-bold'>Projects</h1>
 				</div>
 
 				<Tabs selectedTabClassName='border-b-2 border-transparent border-blue-300'>
-					<TabList className='flex flex-row justify-center mb-3'>
-						<Tab className='mr-3 p-2 block hover:bg-blue-200 rounded'>
+					<TabList className='flex flex-row'>
+						<Tab className='mr-3 px-1 py-2 block hover:bg-blue-200 rounded font-semibold sm:text-base text-sm'>
 							Personal Projects
 						</Tab>
-						<Tab className='mr-3 p-2 block hover:bg-blue-200 rounded'>
+						<Tab className='mr-3 px-1 py-2 block hover:bg-blue-200 rounded font-semibold sm:text-base text-sm'>
 							Academic Projects
 						</Tab>
 					</TabList>
-					<div className='mt-6 '>
+					<div>
 						<TabPanel>
-							<h1 className='text-sm font-semibold text-center'>
-								This is my web development portfolio.
-							</h1>
-							<div className='p-3 mt-3 border-2 border-separate sm:shadow-xl rounded '>
+							<div className='mt-6'>
+								<h1>
+									I'm constantly learning new things.
+									Recently, I've picked up web development.
+									Starting from vanila HTML/CSS/JS to
+									frameworks such as React, here are some of
+									my featured works.
+								</h1>
+							</div>
+
+							<div className='p-3 mt-6 border-2 border-separate sm:shadow-xl rounded '>
 								<div className='md:grid lg:grid-cols-3 md:grid-cols-2'>
 									<div className='md:mr-6 lg:col-span-2'>
 										<img src='./homepage.png' alt='' />
@@ -53,14 +60,14 @@ function Projects() {
 											website has standard e-commerce
 											features such as searching,
 											browsing, add to cart, checkout,
-											login, create account, and many
-											more.
+											login, create account,
+											authentication, etc.
 										</div>
 										<div className='flex flex-wrap text-xs font-mono mt-3'>
 											<p className='mr-3'>
 												HTML/CSS/VanillaJS
 											</p>
-											<p className='mr-3'>BootstrapS</p>
+											<p className='mr-3'>Bootstrap</p>
 											<p className='mr-3'>jQuery</p>
 											<p className='mr-3'>Java EE</p>
 											<p className='mr-3'>SQL</p>
@@ -70,7 +77,7 @@ function Projects() {
 									</div>
 								</div>
 							</div>
-							<div className='p-3 mt-12 border-2 border-separate sm:shadow-xl rounded '>
+							<div className='p-3 mt-6 border-2 border-separate sm:shadow-xl rounded '>
 								Lorem ipsum dolor sit amet consectetur
 								adipisicing elit. Soluta, illo quas. Placeat
 								excepturi qui pariatur deserunt nam odit officia
@@ -79,13 +86,59 @@ function Projects() {
 							</div>
 						</TabPanel>
 						<TabPanel>
-							<p>
-								Lorem ipsum dolor sit amet consectetur,
-								adipisicing elit. Quia saepe deserunt ipsam,
-								nihil atque ad, labore repellendus voluptate a
-								exercitationem facere veritatis ab eos aliquam
-								nisi inventore ullam non enim.
-							</p>
+							<div className='mt-6'>
+								<h1>
+									I'm constantly learning new things.
+									Recently, I've picked up web development.
+									Starting from vanila HTML/CSS/JS to
+									frameworks such as React, here are some of
+									my featured works.
+								</h1>
+							</div>
+
+							<div className='p-3 mt-6 border-2 border-separate sm:shadow-xl rounded '>
+								<div className='md:grid lg:grid-cols-3 md:grid-cols-2'>
+									<div className='md:mr-6 lg:col-span-2'>
+										<img src='./homepage.png' alt='' />
+									</div>
+									<div className='flex flex-col flex-1 justify-evenly'>
+										<div className='flex flex-col md:mt-0 mt-3'>
+											<h1 className='text-xl font-bold'>
+												Buy-a-Movie
+											</h1>
+											<div className='mt-2'>
+												<SocialIcon
+													url='https://github.com/bnluong/buy-a-movie'
+													style={{
+														maxWidth: '2rem',
+														maxHeight: '2rem',
+													}}
+												/>
+											</div>
+										</div>
+										<div className='mt-3'>
+											Buy-a-Movie is a mock e-commerce
+											website that sells movies. The
+											website has standard e-commerce
+											features such as searching,
+											browsing, add to cart, checkout,
+											login, create account,
+											authentication, etc.
+										</div>
+										<div className='flex flex-wrap text-xs font-mono mt-3'>
+											<p className='mr-3'>
+												HTML/CSS/VanillaJS
+											</p>
+											<p className='mr-3'>Bootstrap</p>
+											<p className='mr-3'>jQuery</p>
+											<p className='mr-3'>Java EE</p>
+											<p className='mr-3'>SQL</p>
+											<p className='mr-3'>Tomcat</p>
+											<p className='mr-3'>REST API</p>
+										</div>
+									</div>
+								</div>
+							</div>
 						</TabPanel>
 					</div>
 				</Tabs>
