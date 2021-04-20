53efc59654096c3a7704a2dbd76fb8e02fd42fe5
Bao Luong <bnluong@uci.edu>
2021-04-20 14:22:06 -0700

    Changed about data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -11,7 +11,7 @@ postpone my post-grad job search in order to help taking
 care of my family. During this time, I picked up
 full-stack web development. I really enjoyed learning
 and building full-stack web app from scratch.`,
-	`When not coding, I enjoy playing classical guitar and listening to classical music while reading books.`,
+	`When not coding, I enjoy playing classical guitar, playing video games, and reading about mediaval Europe history.`,
 ];
 export const education = [
 	{
